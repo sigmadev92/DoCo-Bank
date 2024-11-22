f8898f7f32028b172653d071e55b2de878b4efdc
Vaishali Jain <134711372+Vaishalijain10@users.noreply.github.com>
2024-11-22 22:50:42 +0530

    Update URL.js

--- frontend/src/api/URL.js
+++ frontend/src/api/URL.js
@@ -1,7 +1,7 @@
 // api/URL.js
 // export const baseUrl = "http://localhost:1234";
 // new api -> after deployment
-export const baseUrl = "https://doco-bank.vercel.app/";
+export const baseUrl = "https://doco-bank.vercel.app";
 export const userUrl = `${baseUrl}/user`;
 export const accountUrl = `${baseUrl}/account/`;
 
